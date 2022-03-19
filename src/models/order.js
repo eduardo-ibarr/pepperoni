52bf321c361d3cf37eebf62d605480e3f2d7e8d6
eduardo <eduardoibarr56@gmail.com>
2022-03-18 23:14:02 -0300

    Correções da implementação

--- src/models/order.js
+++ src/models/order.js
@@ -1,23 +1,12 @@
 const mongoose = require("mongoose")
 
-const dadosSchema = new mongoose.Schema({
-    nome: { type: String, required: true },
-    endereco: { type: String, required: true },
-    pagamento: { type: String, required: true }
-})
-
-const trocoSchema = new mongoose.Schema({
-    dinheiro: { type: Number, required: false, default: 0 },
-    devolve: { type: Number, required: false, default: 0 }
-})
-
 const orderSchema = new mongoose.Schema(
 	{
         _id: { type: Number, required: true},
         pedidos: { type: Array, required: true, default: []},
         observacoes: { type: String, required: false, default: null },
-        dados: [dadosSchema],
-        troco: [trocoSchema],
+        dados: { type: Array, required: true },
+        troco: [{ type: Array, required: false }],
         feedback: { type: String, required: false, default: null}
 	}
 )
