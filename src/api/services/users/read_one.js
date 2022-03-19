afa3cf9c7b47f4b3675cf2ec95569cc6d3c13489
eduardo <eduardoibarr56@gmail.com>
2022-03-19 10:45:59 -0300

    Correções da implementação

--- src/api/services/users/read_one.js
+++ src/api/services/users/read_one.js
@@ -4,17 +4,16 @@ const connection = require("../../../database/connection")
 
 module.exports = (req, res) => {
     const cpf = req.params.cpf
-    try {
-        connection.then(
-            () => {
-                const user = userSchema.find({_id: cpf}).lean()
-                res.status(200).send(user)
-            },
-            err => {throw err}
-        )
-    } catch(error) {
-        console.error(error)
-        res.status(404).send(notFound)
-    }
+    
+    connection.then(
+        () => {
+            const user = userSchema.find({_id: cpf}).lean()
+            res.status(200).send(user)
+        },
+        err => { 
+            console.error(err)
+            res.status(404).send(notFound)
+        }
+    )
 }
 
