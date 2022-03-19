6484a62ce0a6ddd0ab09c546f295d97b5ea09d14
eduardo <eduardoibarr56@gmail.com>
2022-03-19 14:07:01 -0300

    Correções da implementação

--- src/api/services/pizza/update.js
+++ src/api/services/pizza/update.js
@@ -5,22 +5,24 @@ const connection = require("../../../database/connection")
 module.exports = (req, res) => {
     const id = parseInt(req.params.id)
     const data = req.body
-    try {
-        connection.then(
-            () => {
-                pizzaSchema.findOneAndUpdate({_id: id}, {$set: data});
-                res.status(200).send({
-                    code: 200,
-                    message: `Pizza com id ${id} atualizada com sucesso.`
-                })
-            },
-            err => {throw err}
-        )
-
-    } catch (error) {
-        console.error(error)
-        res.status(400).send(badRequest)
-    }
-
+    connection.then(
+        () => {
+            pizzaSchema.findByIdAndUpdate(id, {$set: data}, (err, data) => {
+                if(err){
+                    console.log(err);
+                }
+                else{
+                    res.status(200).send({
+                        code: 200,
+                        message: `Pizza com id ${id} atualizada com sucesso.`
+                    })                    
+                }
+            })
+        },
+        err => {
+            console.error(err)
+            res.status(400).send(badRequest)
+        }
+    )
 }
 
